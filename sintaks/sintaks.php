8eb6dbfe1b3737dde46fc1d386fcc01c804e9b2a
searchforsakh <skhpratama13@gmail.com>
2026-03-03 00:47:29 +0700

    finished sintaks

--- sintaks/sintaks.php
+++ sintaks/sintaks.php
@@ -13,6 +13,44 @@
 $nama = "Ole Romeny";
 
 // operator aritmatika
+// + - * / %
+$a = 5;
+$b = 4;
+
+// penggabung string
+// , .
+
+// aoperator ssignment
+// =, +=, -=, *=, /=, %=, .=
+$x = 20;
+$x =+ 10;
+echo $x;
+echo "\n";
+
+$nama1 = "Sakha";
+$nama1 .= " "; // mempermudah menggabungkan tipe data tanpa membuat variabel baru
+$nama1 .= "Pratama";
+$nama1 .= 1;
+echo $nama1;
+echo "\n";
+
+// operator perbandingan
+// < > <= >= ==
+var_dump(10 == "10"); // akan true, karena == tidak mengecek tipe datanya, hanya mengecek nilainya 
+var_dump(10<9);
+var_dump(10>=9);
+
+// operator identitas (disarankan menggunakan identitas jika ingin mengecek tipe data)
+// === !==
+var_dump(10 === "10"); // akan false, karena === mengecek tipe datanya bukan nilainya
+
+// operator logika 
+// && || !
+// && = harus dua duanya benar
+// || = jika 1 benar, maka hasilnya benar
+$x = 10;
+var_dump($x < 20 || $x %2 == 0);
+var_dump($x < 9 && $x %2 == 0);
 
 ?>
 <!DOCTYPE html>
@@ -24,6 +62,8 @@ $nama = "Ole Romeny";
 </head>
 <body>
     <h1>Selamat datang <?php print $nama ?></h1> <!-- php didalam html -->
-    <?php echo "<p>ini adalah paragraph</p>" ?> <!-- html didalam php -->
+    <?php 
+    echo "<p>ini adalah paragraph</p>";
+    echo "Hasil perkalian ". $a. " & ", $b, " Adalah ". $a*$b; ?> <!-- html didalam php -->
 </body>
 </html>
