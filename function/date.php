cac6d6e37f11e8d0bba3fda1015cad4aeec1327c
searchforsakh <skhpratama13@gmail.com>
2026-03-05 23:09:18 +0700

    finished date

--- function/date.php
+++ function/date.php
@@ -18,7 +18,7 @@ echo time(); // menampilkan detik dari 1 jan 1970 sampai hari ini
 print "<br>";
 
 // menggabungkan function, menentukan hari ke 31 setelah hari ini
-echo date("l, d-M-Y",time()+60*60*24*31);
+echo date("l, d-M-Y",time()+60*60*24*31); // detik dihari itu diubah oleh date menjadi hari apa
 print "<br>";
 
 // mktime
@@ -27,12 +27,11 @@ mktime(0,0,0,0,0,0); // terdiri dari 6 parameter
 // jam, menit, detik, bulan, tanggal, tahun
 
 // menentukan hari lahir
-echo date("l", mktime(0,0,0,11,6,2006));
+echo date("l", mktime(0,0,0,11,6,2006)); // detik dihari itu diubah oleh date menjadi hari apa
+print "<br>";
 
 
- 
-
-
-
-
+strtotime("10:30pm April 15 2020"); // mengubah string menjadi detik
+echo date("l", strtotime("April 15 2020")); // detik dihari itu, diubah oleh date menjadi hari apa
+print "<br>";
 ?>
