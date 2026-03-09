1a29b5ac6cab2058d68c8b21247dc9520dbcce4b
searchforsakh <skhpratama13@gmail.com>
2026-03-09 19:31:27 +0700

    added get & post

---
+++ GET & POST/superglobals.php
@@ -0,0 +1,6 @@
+<?php
+// SUPERGLOBALS
+// variabel array global milik PHP yang dapat diakses dari mana saja (dalam fungsi, kelas, atau file)
+// bentuknya adalah array associative
+
+echo $_SERVER["HTTP_SEC_CH_UA"];
