307f654076e5d1d29940d5ab2930a83948b35c70
searchforsakh <skhpratama13@gmail.com>
2026-03-13 18:07:43 +0700

    latihan mysql

---
+++ latihan/latihanmysql.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Latihan MYSQL</title>
+</head>
+
+<body>
+    <h1>Daftar Mahasiswa</h1>
+    <table border="1" cellpadding="10" cellspacing="0">
+        <tr>
+            <th>1.</th>
+            <th>Nama</th>
+            <th>NIM</th>
+            <th>Email</th>
+            <th>Kontak</th>
+            <th>Gambar</th>
+        </tr>
+    </table>
+</body>
+
+</html>
